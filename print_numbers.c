f774c4c9c4b2b9aead373b8ebfb0d50224272dde
Thar <user@Thar.local>
2025-10-22 22:11:04 +0700

    added thread

--- print_numbers.c
+++ print_numbers.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 #include<pthread.h>
 
-//pthread_mutex_t mlock;
+pthread_mutex_t mlock;
 
 void* print_num(void* arg){
-    //pthread_mutex_lock(&mlock);
+    pthread_mutex_lock(&mlock);
     for(int i=1; i<=1000; i++){
         printf("%d ",i);
     }
     printf("\n");
-    //pthread_mutex_unlock(&mlock);
+    pthread_mutex_unlock(&mlock);
 
     return NULL;
 }
@@ -17,7 +17,7 @@ void* print_num(void* arg){
 
 int main(){
     pthread_t t1, t2;
-    //pthread_mutex_init(&mlock, NULL);
+    pthread_mutex_init(&mlock, NULL);
     pthread_create(&t1, NULL, print_num, NULL);
     pthread_create(&t2, NULL, print_num, NULL);
 
@@ -25,6 +25,6 @@ int main(){
     pthread_join(t1,NULL );
     pthread_join(t2, NULL);
 
-    //pthread_mutex_destroy(&mlock);
+    pthread_mutex_destroy(&mlock);
     return 0;
 }
